e8ce02399c2b1468822271ea5e3bc47decc516c2
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2019-08-27 22:51:35 +0200

    rewrite the command-line tool with decent user interface
    * bin/main.cc: Add argp for parsing arguments, error() for printing
    messages, and check failures to write to stdout.
    * Makefile.am (bin_tcltl_CPPFLAGS): New variable, so we can use
    gnulib.

--- bin/main.cc
+++ bin/main.cc
@@ -17,6 +17,12 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#include "config.h"
+#include "progname.h"
+#include "argp.h"
+#include "closeout.h"
+#include "error.h"
+
 #include <spot/twaalgos/dot.hh>
 #include <spot/tl/parse.hh>
 #include <spot/tl/print.hh>
@@ -25,94 +31,237 @@
 
 #include "tcltl.hh"
 
+static const char argp_program_doc[] ="\
+Check a timed-automaton against an LTL formula.\v\
+Exit status:\n\
+  0  on success, or if the formula was verified\n\
+  1  if the formula was violated (counter example found)\n\
+  2  if any error has been reported";
+
+// argp's default behavior of offering -? for --help is just too silly.
+// We disable this option as well as -V (because --version doesn't need
+// a short version).
+enum {
+  OPT_HELP = 1,
+  OPT_VARS,
+  OPT_VERSION,
+};
+
+static const argp_option options[] =
+  {
+    { nullptr, 0, nullptr, 0, "Input:", 1 },
+    { "model", 'm', "FILENAME", 0,
+      "read the timed-automaton model in FILENAME (TChecker's syntax)", 0 },
+    { "formula", 'f', "FORMULA", 0,
+      "check the LTL on the model (Spot's syntax)", 0 },
+    { nullptr, 0, nullptr, 0, "Output:", 2 },
+    { "quiet", 'q', nullptr, 0,
+      "suppress standard output (check exit code for result)", 0 },
+    { "dot", 'd', nullptr, 0,
+      "output the result in GraphViz format" },
+    { "vars", OPT_VARS, nullptr, 0,
+      "list variables in the model and exit", 0 },
+    { nullptr, 0, nullptr, 0, "Miscellaneous options:", -1 },
+    { "version", OPT_VERSION, nullptr, 0, "print program version", 0 },
+    { "help", OPT_HELP, nullptr, 0, "print this help", 0 },
+    // We support --usage as a synonym for --help because argp's
+    // hardcoded error message for unknown options mentions it.
+    { "usage", OPT_HELP, nullptr, OPTION_HIDDEN, nullptr, 0 },
+    { nullptr, 0, nullptr, 0, nullptr, 0 }
+  };
+
+static void
+display_version(FILE *stream, struct argp_state*)
+{
+  fputs(program_name, stream);
+  fputs(" " PACKAGE_VERSION "\n\
+\n\
+Copyright (C) 2019  Laboratoire de Recherche et Développement de l'Epita.\n\
+License GPLv3+: \
+GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n\
+This is free software: you are free to change and redistribute it.\n\
+There is NO WARRANTY, to the extent permitted by law.\n", stream);
+}
+
+
+enum output_type_t { OUTPUT_STD, OUTPUT_DOT, OUTPUT_QUIET, OUTPUT_VARS };
+static output_type_t output_type = OUTPUT_STD;
+static std::string input_formula;
+static spot::formula formula_neg;
+static std::string model_filename;
+
+static void parse_formula(std::string f)
+{
+  input_formula = f;
+  spot::parsed_formula pf = spot::parse_infix_psl(f);
+  if (pf.format_errors(std::cerr))
+    error(1, 0, "error parsing formula");
+  formula_neg = spot::formula::Not(pf.f);
+}
+
+static int
+parse_opt(int key, char* arg, struct argp_state* state)
+{
+  // This switch is alphabetically-ordered.
+  switch (key)
+    {
+    case 'd':
+      output_type = OUTPUT_DOT;
+      break;
+    case 'f':
+      parse_formula(arg);
+      break;
+    case 'm':
+      model_filename = arg;
+      break;
+    case 'q':
+      output_type = OUTPUT_QUIET;
+      break;
+    case OPT_HELP:
+      argp_state_help(state, state->out_stream,
+                      // Do not let argp exit: we want to diagnose a
+                      // failure to print --help by closing stdout
+                      // properly.
+                      ARGP_HELP_STD_HELP & ~ARGP_HELP_EXIT_OK);
+      close_stdout();
+      exit(0);
+      break;
+    case OPT_VARS:
+      output_type = OUTPUT_VARS;
+      break;
+    case OPT_VERSION:
+      display_version(state->out_stream, state);
+      close_stdout();
+      exit(0);
+      break;
+    case ARGP_KEY_ARG:
+      if (model_filename.empty())
+        model_filename = arg;
+      else if (input_formula.empty())
+        parse_formula(arg);
+      else
+        error(2, 0, "too many arguments: %s", arg);
+      break;
+    default:
+      return ARGP_ERR_UNKNOWN;
+    }
+  return 0;
+}
+
+static int run()
+{
+  auto dict = spot::make_bdd_dict();
+  tchecker::gc_t gc;
+  tc_model m = tc_model::load(model_filename);
+
+  if (!formula_neg
+      && output_type != OUTPUT_VARS
+      && output_type != OUTPUT_DOT)
+    {
+      std::cout << "No LTL formula specified.\n";
+      output_type = OUTPUT_VARS;
+    }
+
+  if (output_type == OUTPUT_VARS)
+    {
+      m.dump_info(std::cout);
+      return 0;
+    }
+
+  if (!formula_neg && output_type == OUTPUT_DOT)
+    {
+      spot::atomic_prop_set ap;
+      auto k = m.kripke(gc, &ap, dict);
+      gc.start();
+      k->set_named_prop("automaton-name", new std::string(model_filename));
+      spot::print_dot(std::cout, k, ".kvA");
+      gc.stop();
+      return 0;
+    }
+
+  spot::twa_graph_ptr af = spot::translator(dict).run(formula_neg);
+  spot::atomic_prop_set ap;
+  spot::atomic_prop_collect(formula_neg, &ap);
+  spot::twa_ptr k = m.kripke(gc, &ap, dict);
+  gc.start();
+  if (output_type == OUTPUT_DOT)
+    {
+      auto oldk = k;
+      k = spot::make_twa_graph(k, spot::twa::prop_set::all(), true);
+      // Make sure we stop gc before oldk is destroyed.
+      gc.stop();
+    }
+  int exit_code = 0;
+  auto run = k->intersecting_run(af);
+  exit_code = !!run;
+  switch (output_type)
+    {
+    case OUTPUT_STD:
+      if (run)
+        std::cout
+          << "formula is violated by the following run:\n" << *run;
+      else
+        std::cout << "formula is satisfied\n";
+      break;
+    case OUTPUT_QUIET:
+      break;
+    case OUTPUT_DOT:
+      if (run)
+        {
+          run->highlight(5);
+          k->set_named_prop("automaton-name",
+                            new std::string(model_filename +
+                                            "\ncounterexample for "
+                                            + input_formula));
+        }
+      else
+        {
+          k->set_named_prop("automaton-name",
+                            new std::string(model_filename +
+                                            "\nsatisfies "
+                                            + input_formula));
+        }
+      spot::print_dot(std::cout, k, ".kvAn");
+      break;
+    case OUTPUT_VARS:
+      /* unreachable */
+      break;
+    }
+  if (output_type != OUTPUT_DOT)
+    gc.stop();
+  return exit_code;
+}
+
+
 
 int main(int argc, char * argv[])
 {
-  if (argc < 2 || argc > 4) {
-    std::cerr << "Usage: " << argv[0] << " filename [formula] [-D]\n";
-    return 1;
-  }
+  argp_program_bug_address = "<" PACKAGE_BUGREPORT ">";
+  // Simplify the program name, because argp() uses it to report
+  // errors and display help text.
+  set_program_name(argv[0]);
+  argv[0] = const_cast<char*>(program_name);
+  argp_program_version_hook = display_version;
+  argp_err_exit_status = 2;
 
-  auto dict = spot::make_bdd_dict();
-
-  spot::formula f;
-
-  bool dot = false;
-  if (argc > 1 && strncmp(argv[argc - 1], "-D", 3) == 0)
-    {
-      dot = 1;
-      --argc;
-    }
-
-  std::string orig_formula;
-  if (argc >= 3)
-  {
-    // Parse the input formula.
-    orig_formula = argv[2];
-    spot::parsed_formula pf = spot::parse_infix_psl(argv[2]);
-    if (pf.format_errors(std::cerr))
-      return 1;
-    // Translate its negation.
-    f = spot::formula::Not(pf.f);
-  }
+  const argp ap = { options, parse_opt, "[FILENAME] [FORMULA]",
+                    argp_program_doc, nullptr, nullptr, nullptr };
+  if (int err = argp_parse(&ap, argc, argv, ARGP_NO_HELP, nullptr, nullptr))
+    exit(err);
 
   int exit_code = 0;
   try {
-    tchecker::gc_t gc;
-    tc_model m = tc_model::load(std::string(argv[1]));
-    if (f)
-      {
-        spot::twa_graph_ptr af = spot::translator(dict).run(f);
-        spot::atomic_prop_set ap;
-        spot::atomic_prop_collect(f, &ap);
-        spot::twa_ptr k = m.kripke(gc, &ap, dict);
-        if (dot)
-          k = spot::make_twa_graph(k, spot::twa::prop_set::all(), true);
-        gc.start();
-        if (auto run = k->intersecting_run(af))
-          {
-            exit_code = 1;
-            if (!dot)
-              {
-                std::cout
-                  << "formula is violated by the following run:\n" << *run;
-              }
-            else
-              {
-                run->highlight(5);
-                k->set_named_prop("automaton-name",
-                                  new std::string(std::string(argv[1]) +
-                                                  "\ncounterexample for "
-                                                  + orig_formula));
-                spot::print_dot(std::cout, k, ".kvAn");
-              }
-          }
-        else
-          {
-            std::cout << "formula is verified\n";
-          }
-        gc.stop();
-      }
-    else
-      {
-        if (dot)
-          {
-            spot::atomic_prop_set ap;
-            auto k = m.kripke(gc, &ap, dict);
-            gc.start();
-            k->set_named_prop("automaton-name", new std::string(argv[1]));
-            spot::print_dot(std::cout, k, ".kvA");
-            gc.stop();
-          }
-        else
-          {
-            m.dump_info(std::cout);
-          }
-      }
+    exit_code = run();
   }
   catch (const std::exception& e) {
-    std::cerr << e.what();
-    return 2;
+    error(2, 0, e.what());
   }
+
+  // Make sure we abort if we can't write to std::cout anymore
+  // (like disk full or broken pipe with SIGPIPE ignored).
+  std::cout.flush();
+  if (!std::cout)
+    error(2, 0, "error writing to standard output");
+
   return exit_code;
 }
